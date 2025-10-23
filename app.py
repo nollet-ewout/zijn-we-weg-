20d2dcca22199ffb7b5566af0efdee29b664d570
Ewout Nollet <nolletewout@gmail.com>
2025-10-23 16:51:44 +0200

    Update app.py

--- app.py
+++ app.py
@@ -33,7 +33,7 @@ def main():
 
     if st.session_state['needs_refresh']:
         st.session_state['needs_refresh'] = False
-        st.experimental_rerun()
+        st.rerun()
 
     if selected_tab == "Reislocaties":
         data = load_travel_data()
@@ -153,3 +153,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+
